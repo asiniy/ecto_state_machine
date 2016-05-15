e9759d0af728dcde7fca129f78259a50358f2b07
Alex Antonov <kaburbundokel11g@inbox.ru>
2016-05-15 15:48:51 +0300

    Version 0.0.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule EctoStateMachine.Mixfile do
   def project do
     [
       app: :ecto_state_machine,
-      version: "0.0.1",
+      version: "0.0.2",
       elixir: "~> 1.2",
       elixirc_paths: elixirc_paths(Mix.env),
       build_embedded: Mix.env == :prod,
