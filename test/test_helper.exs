c8ec8c5c752cc011de7686cca911c6cb4e7c4a5a
Alex Antonov <alex@fasteria.ru>
2016-07-16 13:59:24 +0300

    Travis CI integration

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,3 +1,7 @@
+Mix.Task.run "ecto.drop",    ["quiet", "-r", "Dummy.Repo"]
+Mix.Task.run "ecto.create",  ["quiet", "-r", "Dummy.Repo"]
+Mix.Task.run "ecto.migrate", ["-r", "Dummy.Repo"]
+
 ExUnit.start()
 
 { :ok, _ } = Dummy.Repo.start_link
